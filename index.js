19dda624e99be6bb27c57df2877d57372e4a7437
Andrey Titok <andrey.titok@gmail.com>
2023-09-14 16:37:07 +0200

    Added telegram bot integration

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const { Telegraf } = require('telegraf');
+
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 
@@ -11,6 +15,18 @@ app.get('/', (req, res) => {
 app.get('/status', (req, res) => {
     res.json({ status: 'OK' });
 });
+
+
+const TGBOT_TOKEN = process.env.TGBOT_TOKEN;
+
+const bot = new Telegraf(TGBOT_TOKEN);
+
+bot.on('message', (ctx) => {
+    ctx.reply('HELLO');
+});
+
+bot.launch();
+
 
 app.listen(port, () => {
     console.log(`App is running on port ${port}`);
